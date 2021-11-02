ae2b2c52ed4f35a93e437998f7e54c621843512f
Brian <briancarnicel@gmail.com>
2021-11-02 16:06:16 -0300

    se creo la vista de la targeta de registro de usuario

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,14 @@
-const express=require('express')
-const router=express.Router()
+const express = require('express')
+const router = express.Router()
+
+//ruta para renderizar el formulario
+router.get("/signup",(req,res) => {
+    res.render("auth/signup");  
+});
+
+//ruta para recibir los datos del formulario
+router.post("/signup",(req,res) => {
+
+});
 
 module.exports=router
